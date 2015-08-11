63358e6c3aa6e131c06af948a6c1230c1ddcd014
liujinlongxa <liujinlongxa@gmail.com>
2015-08-11 22:14:21 +0800

    add pan gesture to dismiss transition

--- testTransitioning/FirstViewController.swift
+++ testTransitioning/FirstViewController.swift
@@ -10,12 +10,51 @@ import UIKit
 
 class FirstViewController: UIViewController, UIViewControllerTransitioningDelegate {
 
+    private var percentDriven:UIPercentDrivenInteractiveTransition?
+    private var beginPoint:CGPoint = CGPointZero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        self.transitioningDelegate = self
+//        let panGesture = UIPanGestureRecognizer(target: self, action: Selector("panGesture:"))
+//        self.view.addGestureRecognizer(panGesture)
+    }
+    
+    func panGesture(gesture:UIPanGestureRecognizer) {
+        
+        let progress = (gesture.locationInView(self.view).x - beginPoint.x) / (self.view.bounds.width - beginPoint.x)
+        print(progress)
+        
+        if gesture.state == UIGestureRecognizerState.Began {
+            beginPoint = gesture.locationInView(self.view)
+            self.percentDriven = UIPercentDrivenInteractiveTransition()
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        else if gesture.state == UIGestureRecognizerState.Changed {
+            self.percentDriven?.updateInteractiveTransition(progress)
+        }
+        else if gesture.state == UIGestureRecognizerState.Cancelled || gesture.state == UIGestureRecognizerState.Ended {
+            if progress > 0.5 {
+                self.percentDriven?.finishInteractiveTransition()
+            }
+            else {
+                self.percentDriven?.cancelInteractiveTransition()
+            }
+            self.percentDriven = nil
+        }
+    }
+    
+    func interactionControllerForDismissal(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return self.percentDriven
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -34,6 +73,8 @@ class FirstViewController: UIViewController, UIViewControllerTransitioningDelega
             
             let secondVC = segue.destinationViewController
             secondVC.transitioningDelegate = self
+            let panGesture = UIPanGestureRecognizer(target: self, action: Selector("panGesture:"))
+            secondVC.view.addGestureRecognizer(panGesture)
             
         }
     }
